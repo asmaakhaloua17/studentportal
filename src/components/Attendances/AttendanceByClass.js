68191ab44fc54492c8aad81cb22e4e7863aca646
anthony <zanthony456@gmail.com>
2021-12-07 12:13:53 -0600

    added backend to attendance page

--- src/components/Attendances/AttendanceByClass.js
+++ src/components/Attendances/AttendanceByClass.js
@@ -1,20 +1,25 @@
 import React, { Component } from "react";
-import { Container, Row, Col, Table } from "react-bootstrap";
+import { Container, Row, Col, Table, Card, ProgressBar } from "react-bootstrap";
 import Footer from "../Footer";
 import Sidenav from "../Sidenav";
 import ClassNav from "../Classes/ClassNav";
 import { getDatabase, ref, get, child } from "firebase/database";
+import AttendancesTableRows from "./AttendancesTableRows";
+let pres = 0;
+let abs = 0;
+let rec = 0;
 
 export default class AttendanceByClass extends Component {
   constructor(props) {
     super(props);
     this.state = { classTitle: "" ,classSection :"" };
+    this.state = { attendsList : [] };
   }
   componentDidMount() {
     const dbRef = ref(getDatabase());
+    let attendList = [];
 
     //get  classe title
-
     get(child(dbRef, "classes/" + this.props.match.params.classId))
       .then(
         (snapshot) => {
@@ -37,11 +42,43 @@ export default class AttendanceByClass extends Component {
       .catch((error) => {
         console.error(error);
       });
+
+      // get attendances
+      get(child(dbRef, "attendances/" + this.props.match.params.classId + "/" + this.props.match.params.euid))
+      .then(attend => {
+          if (attend.exists()) {
+              pres = 0;
+              abs = 0;
+              rec = 0;
+              attend.forEach(item => {
+                  let itemVal = item.val();
+                  attendList.push(itemVal);
+                  console.log(itemVal);
+                  
+                  if(itemVal.status == "Present" ){ 
+                    pres++;
+                  }
+                  else{
+                    abs++;
+                  }
+                  rec++;
+              });
+              
+              this.setState({ attendsList: attendList });
+          } 
+          else {
+              console.log("No attendances found");
+          }
+      }, {
+          onlyOnce: true
+      }).catch(error => {
+          console.log(error);
+      });
   }
 
   render() {
     return (
-      <div>
+      <>
     
        <Sidenav euid={this.props.match.params.euid}/>
         <Container>
@@ -52,7 +89,7 @@ export default class AttendanceByClass extends Component {
             </Col>
             <Col> <h3 className="big_title">Attendances for {this.state.classTitle} Section {this.state.classSection}</h3>
            
-            
+            <Row>
             <Col lg= {6} md ={12}>
                 <Table striped hover size ="sm" className="gradesTable">
                     <thead>
@@ -62,21 +99,20 @@ export default class AttendanceByClass extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>12/12/1212</td>
-                            <td>Present</td>
-                        </tr>
-                        <tr>
-                            <td>Two</td>
-                            <td>temp</td>
-                        </tr>
-                        <tr>
-                            <td>Three</td>
-                            <td>temp</td>
-                        </tr>
+                        <AttendancesTableRows classID={this.props.match.params.classId} euid={this.props.match.params.euid}></AttendancesTableRows>
                     </tbody>
                 </Table>
             </Col>
+            <Col lg={{ span: 4 , offset: 1 }} md={12}>
+                <Card>
+                <h4 className="mb-2 mt-2">Attendance Count</h4>
+                  <h5>Present</h5>
+                  <ProgressBar now={(pres/rec) * 100} label={pres} className="mb-2"/>
+                  <h5>Absent</h5>
+                  <ProgressBar now={(abs/rec) * 100} label={abs} className="mb-2"/>
+                </Card>
+            </Col>
+            </Row>
             </Col>
           </Row>
 
@@ -84,7 +120,7 @@ export default class AttendanceByClass extends Component {
             <Footer></Footer>
           </Row>
         </Container>
-      </div>
+      </>
     );
   }
 }
